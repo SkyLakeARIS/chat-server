b56aa435e44f13a9e9dc29c615ab1145e733080d
SkyLake <bunkete15@gmail.com>
2022-07-19 01:11:09 +0900

    서버 만들기 기능을 위한 Manager클래스 제작
    서버 만들기 기능을 위한 Manager클래스 제작

---
+++ Server/Database/Entities/ChatServerEntity.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Server.Database.Entities
+{
+    public sealed class ChatServerEntity : IMongoEntity
+    {
+	    [BsonId]
+	    public ObjectId entityId { get; set; }
+
+	    public long serverID { get; init; }
+
+	    public string serverName { get; init; }
+
+	    public List<long> userList { get; init; }
+
+    }
+}
---
+++ Server/ServerManager.cs
@@ -0,0 +1,45 @@
+﻿namespace Server;
+
+public class ServerManager
+{
+	private ServerManager _serverManager = new ServerManager();
+	private List<Server> _serverList;
+
+	public ServerManager instance
+	{
+		get
+		{
+			return _serverManager;
+		}
+	}
+
+	public Server GanerateServer()
+	{
+		return null;
+	}
+
+	public Server FindServer(long serverID)
+	{
+		foreach (var server in _serverList)
+		{
+			if (server.GetServerID() == serverID)
+			{
+				return server;
+			}
+		}
+
+		return null;
+	}
+
+	public void RemoveServer(Server server)
+	{
+		_serverList.Remove(server);
+	}
+
+	public void AddServer(Server server)
+	{
+		_serverList.Add(server);
+	}
+
+
+}
