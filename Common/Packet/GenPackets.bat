d85720f13e386c590a43f6b5462539e4abdf034c
SkyLake <bunkete15@gmail.com>
2022-07-23 15:08:38 +0900

    파일 삭제
    의미없는 중복 파일 제거

--- Common/Packet/GenPackets.bat
+++
@@ -1,7 +0,0 @@
-START ../../PacketGenerator/bin/Debug/net6.0/PacketGenerator.exe ../../PacketGenerator/PDL.xml
-XCOPY /Y GenPackets.cs "../../DummyClient/Network"
-XCOPY /Y GenPackets.cs "../../Server/Network"
-XCOPY /Y GenPackets.cs "../../Client/Network"
-XCOPY /Y ClientPacketManager.cs "../../DummyClient/Network"
-XCOPY /Y ServerPacketManager.cs "../../Server/Network"
-XCOPY /Y ClientPacketManager.cs "../../Client/Network"
